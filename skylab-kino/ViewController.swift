c2478c7ce55dcce2aaac39b59bfddba8969c1707
leokvit <1cetoprosto@gmail.com>
2023-02-18 12:26:15 +0200

    створив екран авторизації

--- skylab-kino/ViewController.swift
+++ skylab-kino/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var checkboxAgreeRules: CheckBox!
+    
+    @IBAction func checkboxAgreeRulesPressed(_ sender: Any) {
+        setupLoginButton()
+    }
+    
+    @IBOutlet weak var loginButton: DefaultButton!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        checkboxAgreeRules.isChecked = false
+        setupLoginButton()
     }
-
+    
+    private func setupLoginButton() {
+        if checkboxAgreeRules.isChecked {
+            loginButton.setTitle("Ввійти", for: .normal)
+            loginButton.isEnabled = true
+        } else {
+            loginButton.setTitle("Зареєструватись", for: .normal)
+            loginButton.isEnabled = false
+        }
+    }
 
 }
 
