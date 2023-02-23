4abfd57aff7a0c46ae7c3a02ddc658511915c51e
leokvit <1cetoprosto@gmail.com>
2023-02-23 10:37:50 +0200

    додав екран з жанрами

---
+++ skylab-kino/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  skylab-kino
+//
+//  Created by Леонід Квіт on 23.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ skylab-kino/Models/FilmsModel.swift
@@ -0,0 +1,12 @@
+//
+//  FilmsModel.swift
+//  skylab-kino
+//
+//  Created by Леонід Квіт on 23.02.2023.
+//
+
+import Foundation
+
+struct FilmsModel: Codable {
+    
+}
