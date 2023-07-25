8be9bc2c92796283452488606d02a6de4e46d311
leokvit <1cetoprosto@gmail.com>
2023-07-25 15:05:03 +0300

    перейменував Main.storboard в LoginViewController.storboard

--- skylab-kino/Scenes/LoginScene/LoginViewController.swift
+++ skylab-kino/Scenes/LoginScene/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  skylab-kino
 //
 //  Created by Леонід Квіт on 13.02.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var checkboxAgreeRules: CheckBox!
     
